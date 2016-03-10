366cc930dd906e9d59e591db3ea5f6f1fc986acb
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2016-03-10 15:19:19 +0900

    version 1.0.1a

--- lib/shunkuntype/version.rb
+++ lib/shunkuntype/version.rb
@@ -1,6 +1,6 @@
 require 'fileutils'
 module Shunkuntype
-  VERSION = "1.0.1"
+  VERSION = "1.0.1a"
   data_path = File.join(ENV['HOME'], '.shunkuntype')
 #  SPEED_FILE="./shunkuntype_speed_data.txt"
   SPEED_FILE=File.join(data_path,"speed_data.txt")
