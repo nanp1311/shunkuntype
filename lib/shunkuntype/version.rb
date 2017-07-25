b6fe9f7d28d071bc45f1af56910b8b42f9a6ffc3
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2017-07-25 11:09:07 +0900

    git add version

---
+++ lib/shunkuntype/version.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+require 'tmpdir'
+
+module Shunkuntype
+  VERSION = "1.0.13"
+  data_dir = File.join(ENV['HOME'], '.shunkuntype')
+#  SPEED_FILE="./shunkuntype_speed_data.txt"
+  SPEED_FILE=File.join(data_dir,"speed_data.txt")
+#  TRAIN_FILE="./shunkuntype_training_data.txt"
+  TRAIN_FILE=File.join(data_dir,"training_data.txt")
+  SERVER_FILE=File.join(data_dir,"server_data.txt")
+end
