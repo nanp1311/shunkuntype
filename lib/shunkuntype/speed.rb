d9159de5c6f05004771d322b9243b413569a5c18
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2023-06-22 11:56:18 +0900

    new file:   lib/data/nakata.list 	modified:   lib/shunkuntype/speed.rb

--- lib/shunkuntype/speed.rb
+++ lib/shunkuntype/speed.rb
@@ -35,7 +35,8 @@ EOF
   def mk_random_words
     data=[]
     data_dir=File.expand_path('../../../lib/data', __FILE__)
-    file=open("#{data_dir}/word.list",'r')
+    #    file=open("#{data_dir}/word.list",'r')
+    file=open("#{data_dir}/nakata.list",'r')
     while word=file.gets do
       data << word.chomp
     end
