b9375ba4505d67d75a1081fd00046154a13cb841
nanp <fzd65553@kwansei.ac.jp>
2023-06-24 11:03:41 +0900

    nanp.list

--- lib/shunkuntype/speed.rb
+++ lib/shunkuntype/speed.rb
@@ -61,8 +61,8 @@ EOF
     @number.times do |i|
       print_keyboard()
       puts (i+1).to_s
-      word = data[i]
-#      word = data.match(/(\w+)\s+#(.+)/)[1]
+#      word = data[i]
+      word = data[i].match(/(\w+)\s+#(.+)/)[1]
 #      mean = data.match(/(\w+)\s+#(.+)/)[2]
       count+=word.length
       while line!=word do
