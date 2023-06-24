37bee7644dae0357e0dea323f39aedd33fcd4599
nanp <fzd65553@kwansei.ac.jp>
2023-06-24 11:12:04 +0900

    nanp.list

--- lib/shunkuntype/speed.rb
+++ lib/shunkuntype/speed.rb
@@ -63,10 +63,10 @@ EOF
       puts (i+1).to_s
 #      word = data[i]
       word = data[i].match(/(\w+)\s+#(.+)/)[1]
-#      mean = data.match(/(\w+)\s+#(.+)/)[2]
+      mean = data[i].match(/(\w+)\s+#(.+)/)[2]
       count+=word.length
       while line!=word do
-        puts word
+        puts mean
         p ''
         line=$stdin.gets.chomp
       end
