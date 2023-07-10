12590e3b6e2a48437bdf731ccb5f132327df8422
nanp <fzd65553@kwansei.ac.jp>
2023-07-10 10:57:40 +0900

    record

--- lib/shunkuntype/speed.rb
+++ lib/shunkuntype/speed.rb
@@ -60,6 +60,8 @@ EOF
 
   def exec_speed_check(data)
     data_dir = File.expand_path("../../../lib/data", __FILE__)
+    file_correct = open("#{data_dir}/record_correct.list", "a")
+    file_miss = open("#{data_dir}/record_miss.list", "a")
     print "\n\n" + number.to_s + " words should be cleared."
     print "\nmode[J, E, T]: " # J:単語と意味を両方表示, E:単語の意味のみを表示, T:単語のみを表示
     mode = $stdin.gets.chomp
@@ -92,9 +94,7 @@ EOF
       end
       count += word.length
       while line != word
-        File.open("#{data_dir}/record_miss.list", "a") { |f|
-          f.write("#{word}\n")
-        }
+        file_miss.write("#{word}\n")
         puts word_show
         p ""
         line = $stdin.gets.chomp
